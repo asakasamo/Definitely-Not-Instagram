3146bfcb23fc8e57ac845c8ebf4fcaca38d6f07c
asakasamo <a.sakasamo@gmail.com>
2018-11-02 15:22:36 -0500

    Scaffold the main components

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PhotoGrid = React.createClass({
+   render() {
+      return (
+         <div className="photo-grid">
+            <h2>I'm the photo grid</h2>
+         </div>
+      );
+   }
+});
+
+export default PhotoGrid;
---
+++ client/components/Single.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Single = React.createClass({
+   render() {
+      return (
+         <div className="single-photo">
+            <h2>I'm a single post</h2>
+         </div>
+      );
+   }
+});
+
+export default Single;
