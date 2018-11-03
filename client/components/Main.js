35d9b5d8cbbed5fb290a595051f9c002ee5c49ea
asakasamo <a.sakasamo@gmail.com>
2018-11-02 20:14:42 -0500

    Add functionality for incrementing likes - Apply it to the home page

--- client/components/Main.js
+++ client/components/Main.js
@@ -6,7 +6,7 @@ const Main = React.createClass({
       return (
          <div>
             <h1>
-               <Link to="/">Not Instagram</Link>
+               <Link to="/">Definitely Not Instagram</Link>
             </h1>
             {React.cloneElement(this.props.children, this.props)}
          </div>
--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,5 +1,21 @@
 function posts(state = [], action) {
-   console.log(state, action);
+   console.log("Posts reducer!", state, action);
+
+   switch (action.type) {
+      case "INCREMENT_LIKES":
+         console.log("Incrementing likes");
+         const i = action.index;
+         return [
+            ...state.slice(0, i), // before the one we're updating
+            { ...state[i], likes: state[i].likes + 1 },
+            ...state.slice(i + 1) // after the one we're updating
+         ];
+
+      // return the updated state
+      default:
+         return state;
+   }
+
    return state;
 }
 
