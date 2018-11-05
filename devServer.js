15a3dccf5900f3b9143d9cae42b836df5f831bcd
asakasamo <a.sakasamo@gmail.com>
2018-11-04 19:09:40 -0600

    Update dev server config

--- devServer.js
+++ devServer.js
@@ -20,11 +20,11 @@ app.get("*", function(req, res) {
    res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(port, "localhost", function(err) {
+app.listen(port, function(err) {
    if (err) {
       console.log(err);
       return;
    }
 
-   console.log("Listening at http://localhost:" + port);
+   console.log("Listening at port " + port);
 });
